75b58ec131192cc9265b60338c07028dcb5bde68
Nathan Gillespie <112279301+CrookedTwig4@users.noreply.github.com>
2023-11-12 21:16:22 -0800

    <3

--- Travel_Guide/JS/keyReader.js
+++ Travel_Guide/JS/keyReader.js
@@ -22,5 +22,5 @@ document.addEventListener('keydown', function(event) {
 });
 
 function authenticated() {
-    window.location.href = "../../Game_Hub";
+    window.location.href = "crookedtwig4.github.io/Travel_Guide/Game_Hub";
 }
